65ba85aa487d9481376a1705c42313e7fe00c2f2
Lukas <l.dirzys@yahoo.com>
2013-09-29 23:51:06 +0100

    Separated read_file function from all objects to two different files

--- main.py
+++ main.py
@@ -1,65 +1,6 @@
+import read_file as read
+import objects
 
-class Route:
-    'Class for all routes'
-    routeCount = 0
-    
-    def __init__(self, number, stops, buses, capacity):
-        self.number = number
-        self.stops = stops
-        self.buses = buses
-        self.capacity = capacity
-        Route.routeCount += 1
-        
-class Road:
-    'Class for all roads'
-    roadCount = 0
-    
-    def __init__(self, starts, ends, rate):
-        self.starts = starts
-        self.ends = ends
-        self.rate = rate
-        Road.roadCount += 1
-        
-#Initialize variables
-routes = []
-roads = []
-boardRate = 0
-disembarkRate = 0
-busDepartRate = 0
-paxArrivalRate = 0
-stopTime = 0
-ignoreWarnings = False
-optimiseParameters = False
- 
-def processLine(line):
-    data = line.split(" ")
-    object = data[0]
-    if object == "route":
-        stops = []
-        for i in range(3, len(data)-4):
-            stops.append(data[i])    
-        routes.append(Route(data[1], stops, data[len(data)-3], data[len(data)-1]))
-    if object == "road":
-        roads.append(Road(data[1], data[2], data[3]))
-    if object == "board":
-        boardRate = data[1]
-    if object == "disembarks":
-        disembarkRate = data[1]
-    if object == "departs":
-        busDepartRate = data[1]
-    if object == "new":
-        paxArrivalRate = data[2]
-    if object == "stop":
-        stopTime = data[2]
-    if object == "ignore":
-        ignoreWarnings = True
-    if object == "optimise":
-        optimiseParameters = True
-    return 
+initialState = read.readFromFile('input.dat')
 
-file = open('input.dat', 'r')
-
-for line in file:
-    processLine(line)
-    
-file.close()
+print initialState

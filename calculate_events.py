1e04bbf45169aee42125ed8694240337cd8564ed
Lukas <l.dirzys@yahoo.com>
2013-10-17 23:08:34 +0100

    Changed the representation of possible events to tuple type

--- calculate_events.py
+++ calculate_events.py
@@ -6,9 +6,9 @@ def canBoardBus(state):
 
 def calculate(state):
     #Find all possible events with their rates
-    possibleEvents = {}
+    possibleEvents = []
     #New passenger can always arrive into the random stop with rate paxArrivalRate
-    possibleEvents[state.paxArrives] = ['newpax']
+    possibleEvents.append((state.paxArrives, ['newpax']))
     #Find all passengers who can board some bus and add them to possible events list
     boardingEvents = canBoardBus(state)
     return possibleEvents
--- main.py
+++ main.py
@@ -3,18 +3,20 @@ import objects
 import calculate_events
 import math
 from random import random
-import operator
 
 initialState = createNetwork.readFromFile('input.dat')
 
 time = 0
 while time <= float(initialState.stopTime):
-    rates = calculate_events.calculate(initialState)
-    print rates
-    events = {'a': 0.1}
-    totalRate = sum([events[event] for event in events])
+    events = calculate_events.calculate(initialState)
+    print events
+    totalRate = sum([event[0] for event in events])
     delay = -totalRate/len(events) * math.log(random())
-    chooseEvent = max(events.iteritems(), key = operator.itemgetter(1))[0]
+    maxRate = max([event[0] for event in events])
+    for event in events: 
+        if event[0] == maxRate: 
+            chooseEvent = event[1]
+            break 
     #modify_state(chooseEvent)
     time = time + float(initialState.stopTime) + 1
     #time = time + delay

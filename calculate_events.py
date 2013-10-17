e1e3e54ea8c37fb1981558a4f40c818e63dee36b
Lukas <l.dirzys@yahoo.com>
2013-10-17 22:33:09 +0100

    Adding initial function to find all passengers who can board some bus

--- calculate_events.py
+++ calculate_events.py
@@ -1,9 +1,14 @@
 import objects as new
 from collections import deque
 
+def canBoardBus(state):
+    return []
+
 def calculate(state):
     #Find all possible events with their rates
     possibleEvents = {}
     #New passenger can always arrive into the random stop with rate paxArrivalRate
     possibleEvents[state.paxArrives] = ['newpax']
+    #Find all passengers who can board some bus and add them to possible events list
+    boardingEvents = canBoardBus(state)
     return possibleEvents

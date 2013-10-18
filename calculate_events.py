f097a43587ebfb163d7ad23422a2e19b256c3ac6
Lukas <l.dirzys@yahoo.com>
2013-10-18 20:40:04 +0100

    Implemented canDisembarkBus function which returns all possible events where some passenger can disembark bus

--- calculate_events.py
+++ calculate_events.py
@@ -10,6 +10,17 @@ def canBoardBus(state):
                 events.append((boardingRate, ['boards', pax, stop]))
     return events
 
+def canDisembarkBus(state):
+    disembarkRate = state.disembarks
+    events = []
+    for stop in state.stops:
+        for bus in stop.busQueue:
+            for pax in bus.passengers:
+                if pax.destination == stop.id:
+                    events.append((disembarkRate, ['disembarks', pax, bus, stop]))
+    
+    return events
+
 def calculate(state):
     #Find all possible events with their rates
     possibleEvents = []
@@ -17,4 +28,6 @@ def calculate(state):
     possibleEvents.append((state.paxArrives, ['newpax']))
     #Find all passengers who can board some bus and add them to possible events list
     possibleEvents.extend(canBoardBus(state))
+    #Find all passengers who can disembark some bus and add these events to possible events list
+    possibleEvents.extend(canDisembarkBus(state))
     return possibleEvents

31c8bd03f44d8224166fe80c1c2249cb38bb381a
Lukas <l.dirzys@yahoo.com>
2013-10-19 23:23:02 +0100

    Removed capacity parameter from Route class

--- objects.py
+++ objects.py
@@ -54,11 +54,10 @@ class Route:
     'Class for all routes'
     routeCount = 0
     
-    def __init__(self, number, stops, buses, capacity):
+    def __init__(self, number, stops, buses):
         self.number = number
         self.stops = stops
         self.buses = buses
-        self.capacity = capacity
         Route.routeCount += 1
         
 class Road:

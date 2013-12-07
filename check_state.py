8a886738f881cb9ce720b1d1eb83e53bdb582c8f
Lukas <l.dirzys@yahoo.com>
2013-12-07 13:05:50 +0000

    Checking if there some unnecessary roads in the input that will not be used in simulation

--- check_state.py
+++ check_state.py
@@ -1,6 +1,28 @@
 
+def requiredRoads(state):
+    roads = []
+    for route in state.routes:
+        for i in range(0, len(route.stops)):
+            road = [route.stops[i], route.stops[(i+1) % len(route.stops)]]
+            if not road in roads:
+                roads.append(road)
+    return roads
+
+def unnecessaryRoads(state):
+    warnings = []
+    required = requiredRoads(state)
+    for road in state.roads:
+        if [road.starts, road.ends] not in required:
+            warnings.append('Warning! Road between stops %(one)s and %(two)s will not be used since no route operates through this road' % \
+                            {'one': road.starts, 'two': road.ends})
+    return warnings
+
 def findWarnings(state):
-    return []
+    warnings = []
+    warnings.extend(unnecessaryRoads(state))
+    
+    return warnings
 
 def findErrors(state):
-    return []
+    errors = []
+    return errors
